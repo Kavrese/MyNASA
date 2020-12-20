673b34aa7dfa5e3bfc744865762ba59fe766b9f9
Артемий <va2005art@gmail.com>
2020-12-20 21:54:11 +0300

    add sort date

--- app/src/main/java/com/example/mynasa/FragmentNews.kt
+++ app/src/main/java/com/example/mynasa/FragmentNews.kt
@@ -18,7 +18,7 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 class FragmentNews: Fragment() {
-    val list_ = mutableListOf<ModelNews>()
+    var list_ = mutableListOf<ModelNews>()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -30,7 +30,7 @@ class FragmentNews: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         rec_news.apply {
             adapter = NewsAdapterRec(list_)
-            layoutManager = LinearLayoutManager(activity!!.applicationContext)
+            layoutManager = LinearLayoutManager(activity!!.applicationContext, LinearLayoutManager.HORIZONTAL, false)
         }
 
         val calendar = Calendar.getInstance()
@@ -55,6 +55,7 @@ class FragmentNews: Fragment() {
             override fun onResponse(call: Call<ModelNews>, response: Response<ModelNews>) {
                 if (response.body() != null) {
                     list_.add(response.body()!!)
+                    list_ = sortDate(list_)
                     rec_news.adapter!!.notifyDataSetChanged()
                 }else{
                     Toast.makeText(requireContext(), "Ответ Null", Toast.LENGTH_LONG).show()
@@ -62,5 +63,23 @@ class FragmentNews: Fragment() {
             }
 
         })
+    }
+    private fun sortDate (list: MutableList<ModelNews>): MutableList<ModelNews>{
+        if (list.size > 1) {
+            for (i in 0..list.size - 1) {
+                for (k in (i + 1)..list.size - 1) {
+                    val sdf = SimpleDateFormat("yyyy-MM-dd")
+                    val date_k = sdf.parse(list[k].date)
+                    val date_i = sdf.parse(list[i].date)
+                    if (date_i < date_k) {
+                        val dop = list[i]
+                        list[i] = list[k]
+                        list[k] = dop
+                        break
+                    }
+                }
+            }
+        }
+        return list
     }
 }
--- app/src/main/java/com/example/mynasa/NewsAdapterRec.kt
+++ app/src/main/java/com/example/mynasa/NewsAdapterRec.kt
@@ -1,18 +1,24 @@
 package com.example.mynasa
 
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
+import java.text.SimpleDateFormat
+import java.util.*
 
 class NewsAdapterRec(private val list: MutableList<ModelNews>): RecyclerView.Adapter<NewsAdapterRec.MyViewHolder>() {
     class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
         val img_news = itemView.findViewById<ImageView>(R.id.img_news)
         val info_news = itemView.findViewById<TextView>(R.id.info_news)
         val tint_news = itemView.findViewById<TextView>(R.id.tint_news)
+        val media_news = itemView.findViewById<TextView>(R.id.type_media)
+        val date_news = itemView.findViewById<TextView>(R.id.date_media)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
@@ -22,15 +28,28 @@ class NewsAdapterRec(private val list: MutableList<ModelNews>): RecyclerView.Ada
 
     override fun getItemCount(): Int = list.size
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         Picasso.get()
             .load(list[position].hdurl)
             .placeholder(R.drawable.load)
             .into(holder.img_news)
         holder.tint_news.text = list[position].title
-        if (list[position].explanation!!.length > 64){
-            list[position].explanation = list[position].explanation!!.substring(0, 64) + "..."
+        if (list[position].explanation!!.length > 128) {
+            list[position].explanation = list[position].explanation!!.substring(0, 128) + "..."
         }
         holder.info_news.text = list[position].explanation
+        holder.media_news.text = list[position].media_type
+        holder.date_news.text = list[position].date
     }
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return position
+    }
+
+
+
 }
